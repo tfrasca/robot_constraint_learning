d67c9e3d0464603fce6c8317835cc56a559ce2e3
tmfrasca <tyfrasc15@gmail.com>
2019-05-10 11:28:22 -0400

    added additional reward for closeness of hands to box

--- robot_constraint_learning/scripts/RLEnv.py
+++ robot_constraint_learning/scripts/RLEnv.py
@@ -1,4 +1,5 @@
 import rospy
+from rospy import ServiceException
 from gazebo_ros import gazebo_interface
 from gazebo_msgs.srv import DeleteModel
 from geometry_msgs.msg import Point, Pose, Quaternion
@@ -12,9 +13,11 @@ import matplotlib.pyplot as plt
 import Robbie
 import Policy
 from simple_dqn_agent import SimpleDQN
+import time
 
 class RLEnv:
-  def __init__(self, stateSpace, actionSpace, nameSpace = "robotis_op3", robotType = None, seed=1, numEpisodes=1000, numSteps=20000):
+  def __init__(self, stateSpace, actionSpace, nameSpace = "robotis_op3", robotType = None, seed=2, numEpisodes=10000, numSteps=500):
+
     #self.unpause = rospy.ServiceProxy("gazebo/unpause_physics", Empty)
     np.random.seed(seed)
     self.robot = Robbie.Robbie(nameSpace)
@@ -29,43 +32,65 @@ class RLEnv:
     self.actionJointNames = actionSpace[0]
     self.actionJointControl = actionSpace[1]
     self.numActions = len(self.actionJointControl[0])**len(self.actionJointNames)
-    print 'num actions:', self.numActions
+    #print 'num actions:', self.numActions
     # change the policy type based on the state and action spaces
     #self.policy = Policy.Policy(stateSpace, actionSpace)
-    self.policy = SimpleDQN(self.numActions, 18, 2, .01, .99, .99, .2, 1)
+    self.epsilonMax =.5
+    self.epsilonMin =0.05
+    self.policy = SimpleDQN(self.numActions, 18, 7, .01, .99, .99, self.epsilonMax, 1)
     self.actions = []
     self.createActionSpace()
     self.rewards=[]
-    self.numSteps =[]
- 
-  def explore(self):
+    self.episodeSteps =[]
     try:
-      for episode in range(self.numEpisodes):
-        R = 0
-        print "episode: ", episode
-        for x in xrange(10):
+      rospy.ServiceProxy("gazebo/unpause_physics",Empty).call()
+    except ServiceException:
+      pass
+
+  def run(self, maxSteps=1000, explore = False):
+      R = 0
+      numSteps = 0
+      while not self.fallen and numSteps < maxSteps:
+        numSteps += 1
+        initState = self.getState()
+        #act = self.policy.selectAction(initState)
+        act = self.policy.process_step(initState, explore)
+        action = self.actions[act]
+        #print "moving joints relative", action
+        self.executeAction(action)
+        newState = self.getState()
+        reward = self.calcReward(newState, action)
+        self.policy.give_reward(reward)
+        R += reward
+        #self.policy.updatePolicy(initState, newState, reward)
+      return (numSteps, R)
+
+  def explore(self):
+    episode = 0
+    print "exploring!"
+    try:
+      for episode in range(1,self.numEpisodes):
+        t0 = time.time()
+        while True:
           self.resetEnv()
           self.checkFallen()
           if not self.fallen:
             break
-        numSteps = 0
-        while not self.fallen and numSteps < self.numSteps:
-          numSteps += 1
-          initState = self.getState()
-          print numSteps
-          #act = self.policy.selectAction(initState)
-          act = self.policy.process_step(initState, True)
-          action = self.actions[act]
-          print "moving joints relative", action
-          self.executeAction(action)
-          newState = self.getState()
-          reward = self.calcReward(newState, action)
+        print "episode: ", episode
+        epsilon = self.epsilonMin + (self.epsilonMax - self.epsilonMin) * np.exp(episode * (np.log(0.01) / (self.numSteps * (3/4.0))))
+        print "epsilon: ", epsilon
+        self.policy.set_explore_epsilon(epsilon)
+        if episode % 5 == 0:
+          print "no exploration"
+          steps,reward = self.run(explore = False)
+          self.episodeSteps.append(steps)
+          self.rewards.append(reward)
+          print "steps:", steps
           print "reward:", reward
-          self.policy.give_reward(reward)
-          R += reward
-          #self.policy.updatePolicy(initState, newState, reward)
-        self.numSteps.append(numSteps)
-        self.rewards.append(R)
+        else:
+          self.run(explore = True)
+        lapsedTime = time.time() - t0
+        print "time", lapsedTime
         self.policy.finish_episode()
         self.policy.update_parameters()
     except KeyboardInterrupt as e:
@@ -78,9 +103,8 @@ class RLEnv:
     self.robot.resetRobot()
     rospy.sleep(3)
     if self.currentModel:
-      self.deleteModel(self.currentModel)
+      x = self.deleteModel(self.currentModel)
       self.currentModel = None
-    #if np.random.randint(2):  # randomly spawn box weight
     if np.random.randint(1):  # spawn light box
       self.spawnModel("heavy_box")
     else:
@@ -106,8 +130,11 @@ class RLEnv:
     self.checkFallen()
     if not self.fallen:
       position = self.robot.getPelvisPosition()
+      reward = np.exp(1.0/(3*position.z))
       #print position.z
-      return np.exp(1.0/(2*position.z))
+      dist = self.robot.getHandBoxDistance()
+      reward += np.exp(1.0/(4*dist))
+      return reward
     return -200
 
   def executeAction(self, action):
@@ -160,24 +187,23 @@ class RLEnv:
             for c4 in control:
               for c5 in control:
                 self.actions.append([c0,c1,c2,c3,c4,c5])
-    
-if __name__ == "__main__":
-  try:
-    jointSpace = ["l_hip_pitch", "l_knee", "l_ank_pitch", "l_sho_pitch", "l_sho_roll", "l_el"]
-    jointState = ["position","velocity","effort"]
-    jointStateStepSize = None
-    stateSpace = (jointSpace, jointState, jointStateStepSize)
-    actionJoints = ["l_hip_pitch", "l_knee", "l_ank_pitch", "l_sho_pitch", "l_sho_roll", "l_el"]
-    actionValues = 6*[[-0.015, 0.0, 0.015]]
-    actionSpace = (actionJoints, actionValues)
-    env = RLEnv(stateSpace, actionSpace)
-    env.explore()
-    rewards = env.rewards
-    numSteps = env.numSteps
+
+  def saveData(self,rewards,numSteps):
     with open('data.csv', 'a') as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
         writer.writerow(rewards)
         writer.writerow(numSteps)
 
-  except KeyboardInterrupt as e:
-    print "quitting"
+if __name__ == "__main__":
+  jointSpace = ["l_hip_pitch", "l_knee", "l_ank_pitch", "l_sho_pitch", "l_sho_roll", "l_el"]
+  jointState = ["position","velocity","effort"]
+  jointStateStepSize = None
+  stateSpace = (jointSpace, jointState, jointStateStepSize)
+  actionJoints = ["l_hip_pitch", "l_knee", "l_ank_pitch", "l_sho_pitch", "l_sho_roll", "l_el"]
+  actionValues = 6*[[-0.015, 0.0, 0.015]]
+  actionSpace = (actionJoints, actionValues)
+  env = RLEnv(stateSpace, actionSpace)
+  env.explore()
+  rewards = env.rewards
+  numSteps = env.episodeSteps
+  env.saveData(rewards,numSteps)
--- robot_constraint_learning/scripts/Robbie.py
+++ robot_constraint_learning/scripts/Robbie.py
@@ -2,163 +2,180 @@ import rospy
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 from gazebo_msgs.msg import LinkStates
+from gazebo_msgs.srv import GetLinkState
+
+import numpy as np
 
 class Robbie:
 
-    def __init__(self, nameSpace='robotis_op3', *kwargs):
-        self.node = rospy.init_node('robbie_' + nameSpace)
-        self.nameSpace = '/' + nameSpace
-        self.setupPubSubs()
-        self.setupJoints()
-        self.actionNameToRL = {}
-        self.jointState = None
-        self.linkStates = None
-        return
+  def __init__(self, nameSpace='robotis_op3', *kwargs):
+    self.node = rospy.init_node('robbie_' + nameSpace)
+    self.nameSpace = '/' + nameSpace
+    self.setupPubSubs()
+    self.setupServices()
+    self.setupJoints()
+    self.actionNameToRL = {}
+    self.jointState = None
+    self.linkStates = None
+    return
 
-    def setupPubSubs(self):
-        self.legTrajPub = rospy.Publisher(self.nameSpace + '/leg_traj_controller/command', JointTrajectory, queue_size=1)
-        self.armTrajPub = rospy.Publisher(self.nameSpace + '/arm_traj_controller/command', JointTrajectory, queue_size=1)
-        self.jointStateSub = rospy.Subscriber(self.nameSpace + '/joint_states', JointState, self.jointStateCallback)
-        self.gazeboLinkSub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.linkStatesCallback)
+  def setupPubSubs(self):
+    self.legTrajPub = rospy.Publisher(self.nameSpace + '/leg_traj_controller/command', JointTrajectory, queue_size=1)
+    self.armTrajPub = rospy.Publisher(self.nameSpace + '/arm_traj_controller/command', JointTrajectory, queue_size=1)
+    self.jointStateSub = rospy.Subscriber(self.nameSpace + '/joint_states', JointState, self.jointStateCallback)
+    self.gazeboLinkSub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.linkStatesCallback)
 
-    def setupJoints(self):
-        self.legJoints = [
-         'l_hip_pitch', 'l_hip_roll', 'l_hip_yaw', 'l_knee', 'l_ank_pitch', 'l_ank_roll', 'r_hip_pitch', 'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_ank_roll', 'r_ank_pitch']
-        self.legPositions = 12 * [0.0]
-        self.legVelocities = 12 * [0.0]
-        self.legEfforts = 12 * [10]
-        self.armJoints = ['l_sho_roll', 'l_sho_pitch', 'l_el', 'r_sho_roll', 'r_sho_pitch', 'r_el']
-        self.armPositions = 6 * [0.0]
-        self.armVelocities = 6 * [0.0]
-        self.armEfforts = 6 * [10]
+  def setupServices(self):
+    self.gazeboLinkSrv = rospy.ServiceProxy("/gazebo/get_link_state", GetLinkState)
 
-    def setLegJoint(self, jointName, pos=None):
-        if pos is not None:
-            index = self.legJoints.index(jointName)
-            self.legPositions[index] = pos
-        return
+  def setupJoints(self):
+    self.legJoints = [
+     'l_hip_pitch', 'l_hip_roll', 'l_hip_yaw', 'l_knee', 'l_ank_pitch', 'l_ank_roll', 'r_hip_pitch', 'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_ank_roll', 'r_ank_pitch']
+    self.legPositions = 12 * [0.0]
+    self.legVelocities = 12 * [0.0]
+    self.legEfforts = 12 * [10]
+    self.armJoints = ['l_sho_roll', 'l_sho_pitch', 'l_el', 'r_sho_roll', 'r_sho_pitch', 'r_el']
+    self.armPositions = 6 * [0.0]
+    self.armVelocities = 6 * [0.0]
+    self.armEfforts = 6 * [10]
 
-    def setArmJoint(self, jointName, pos=None):
-        if pos is not None:
-            index = self.armJoints.index(jointName)
-            self.armPositions[index] = pos
-        return
+  def setLegJoint(self, jointName, pos=None):
+    if pos is not None:
+      index = self.legJoints.index(jointName)
+      self.legPositions[index] = pos
+    return
 
-    def setSymetricLeg(self, jointNames, positions):
-        try:
-            for joint, position in zip(jointNames, positions):
-                if joint == 'l_hip_pitch':
-                    self.setLegJoint(joint, -1 * position)
-                    self.setLegJoint('r_hip_pitch', position)
-                elif joint == 'l_knee':
-                    self.setLegJoint(joint, position)
-                    self.setLegJoint('r_knee', -1 * position)
-                elif joint == 'l_ank_pitch':
-                    self.setLegJoint(joint, position)
-                    self.setLegJoint('r_ank_pitch', -1 * position)
+  def setArmJoint(self, jointName, pos=None):
+    if pos is not None:
+      index = self.armJoints.index(jointName)
+      self.armPositions[index] = pos
+    return
 
-        except Exception as e:
-            print e
+  def setSymetricLeg(self, jointNames, positions):
+    try:
+      for joint, position in zip(jointNames, positions):
+        if joint == 'l_hip_pitch':
+          self.setLegJoint(joint, -1 * position)
+          self.setLegJoint('r_hip_pitch', position)
+        elif joint == 'l_knee':
+          self.setLegJoint(joint, position)
+          self.setLegJoint('r_knee', -1 * position)
+        elif joint == 'l_ank_pitch':
+          self.setLegJoint(joint, position)
+          self.setLegJoint('r_ank_pitch', -1 * position)
 
-    def setRelativeSymetricLeg(self, jointNames, positions):
-        try:
-            currentPositions = self.jointState.position
-            for joint, position in zip(jointNames, positions):
-                position += currentPositions[jointNames.index(joint)]
-                if joint == 'l_hip_pitch':
-                    self.setLegJoint(joint, -1 * position)
-                    self.setLegJoint('r_hip_pitch', position)
-                elif joint == 'l_knee':
-                    self.setLegJoint(joint, position)
-                    self.setLegJoint('r_knee', -1 * position)
-                elif joint == 'l_ank_pitch':
-                    self.setLegJoint(joint, position)
-                    self.setLegJoint('r_ank_pitch', -1 * position)
+    except Exception as e:
+      print e
 
-        except Exception as e:
-            print e
+  def setRelativeSymetricLeg(self, jointNames, positions):
+    try:
+      currentPositions = self.jointState.position
+      for joint, position in zip(jointNames, positions):
+        position += currentPositions[jointNames.index(joint)]
+        if joint == 'l_hip_pitch':
+          self.setLegJoint(joint, -1 * position)
+          self.setLegJoint('r_hip_pitch', position)
+        elif joint == 'l_knee':
+          self.setLegJoint(joint, position)
+          self.setLegJoint('r_knee', -1 * position)
+        elif joint == 'l_ank_pitch':
+          self.setLegJoint(joint, position)
+          self.setLegJoint('r_ank_pitch', -1 * position)
 
-    def setSymetricArm(self, jointNames, positions):
-        try:
-            for joint, position in zip(jointNames, positions):
-                if joint == 'l_sho_pitch':
-                    self.setArmJoint(joint, -1 * position)
-                    self.setArmJoint('r_sho_pitch', position)
-                elif joint == 'l_sho_roll':
-                    self.setArmJoint(joint, position)
-                    self.setArmJoint('r_sho_roll', -1 * position)
-                elif joint == 'l_el':
-                    self.setArmJoint(joint, -1 * position)
-                    self.setArmJoint('r_el', position)
+    except Exception as e:
+      print e
 
-        except Exception as e:
-            print e
+  def setSymetricArm(self, jointNames, positions):
+    try:
+      for joint, position in zip(jointNames, positions):
+        if joint == 'l_sho_pitch':
+          self.setArmJoint(joint, -1 * position)
+          self.setArmJoint('r_sho_pitch', position)
+        elif joint == 'l_sho_roll':
+          self.setArmJoint(joint, position)
+          self.setArmJoint('r_sho_roll', -1 * position)
+        elif joint == 'l_el':
+          self.setArmJoint(joint, -1 * position)
+          self.setArmJoint('r_el', position)
 
-    def setRelativeSymetricArm(self, jointNames, positions):
-        try:
-            currentPositions = self.jointState.position
-            jointNames = self.jointState.name
-            for joint, position in zip(jointNames, positions):
-                position += currentPositions[jointNames.index(joint)]
-                if joint == 'l_sho_pitch':
-                    self.setArmJoint(joint, -1 * position)
-                    self.setArmJoint('r_sho_pitch', position)
-                elif joint == 'l_sho_roll':
-                    self.setArmJoint(joint, position)
-                    self.setArmJoint('r_sho_roll', -1 * position)
-                elif joint == 'l_el':
-                    self.setArmJoint(joint, -1 * position)
-                    self.setArmJoint('r_el', position)
+    except Exception as e:
+      print e
 
-        except Exception as e:
-            print e
+  def setRelativeSymetricArm(self, jointNames, positions):
+    try:
+      currentPositions = self.jointState.position
+      jointNames = self.jointState.name
+      for joint, position in zip(jointNames, positions):
+        position += currentPositions[jointNames.index(joint)]
+        if joint == 'l_sho_pitch':
+          self.setArmJoint(joint, -1 * position)
+          self.setArmJoint('r_sho_pitch', position)
+        elif joint == 'l_sho_roll':
+          self.setArmJoint(joint, position)
+          self.setArmJoint('r_sho_roll', -1 * position)
+        elif joint == 'l_el':
+          self.setArmJoint(joint, -1 * position)
+          self.setArmJoint('r_el', position)
 
-    def resetRobot(self):
-        self.legPositions = 12 * [0.0]
-        self.armPositions = 6 * [0.0]
-        self.move()
+    except Exception as e:
+      print e
 
-    def setEffort(self, jointName, effort):
-        index = self.legJoints.index(jointName)
-        self.legEfforts[index] = effort
+  def resetRobot(self):
+    self.legPositions = 12 * [0.0]
+    self.armPositions = 6 * [0.0]
+    self.move()
 
-    def moveLeg(self):
-        trajPoint = JointTrajectoryPoint(positions=self.legPositions, effort=self.legEfforts, time_from_start=rospy.Duration.from_sec(1))
-        trajMsg = JointTrajectory(joint_names=self.legJoints, points=[trajPoint])
-        self.legTrajPub.publish(trajMsg)
+  def setEffort(self, jointName, effort):
+    index = self.legJoints.index(jointName)
+    self.legEfforts[index] = effort
 
-    def moveArm(self):
-        trajPoint = JointTrajectoryPoint(positions=self.armPositions, effort=self.armEfforts, time_from_start=rospy.Duration.from_sec(1))
-        trajMsg = JointTrajectory(joint_names=self.armJoints, points=[trajPoint])
-        self.armTrajPub.publish(trajMsg)
+  def moveLeg(self):
+    trajPoint = JointTrajectoryPoint(positions=self.legPositions, effort=self.legEfforts, time_from_start=rospy.Duration.from_sec(1))
+    trajMsg = JointTrajectory(joint_names=self.legJoints, points=[trajPoint])
+    self.legTrajPub.publish(trajMsg)
 
-    def move(self):
-        self.moveLeg()
-        self.moveArm()
-        rospy.sleep(2)
+  def moveArm(self):
+    trajPoint = JointTrajectoryPoint(positions=self.armPositions, effort=self.armEfforts, time_from_start=rospy.Duration.from_sec(1))
+    trajMsg = JointTrajectory(joint_names=self.armJoints, points=[trajPoint])
+    self.armTrajPub.publish(trajMsg)
 
-    def doAction(self):
-        policy = self.actionNameToRL(actionName)
-        while not policy.isTerminated():
-            act = policy.getStep(self.getState())
+  def move(self):
+    self.moveLeg()
+    self.moveArm()
+    rospy.sleep(2)
 
-    def jointStateCallback(self, jointState):
-        self.jointState = jointState
+  def doAction(self):
+    policy = self.actionNameToRL(actionName)
+    while not policy.isTerminated():
+      act = policy.getStep(self.getState())
 
-    def getState(self):
-        return self.jointState
+  def jointStateCallback(self, jointState):
+    self.jointState = jointState
 
-    def getLegJoints(self):
-        return self.legJoints
+  def getState(self):
+    return self.jointState
 
-    def getArmJoints(self):
-        return self.armJoints
+  def getLegJoints(self):
+    return self.legJoints
 
-    def linkStatesCallback(self, linkStates):
-        self.linkStates = linkStates
-        states = linkStates
+  def getArmJoints(self):
+    return self.armJoints
 
-    def getPelvisPosition(self):
-        names = self.linkStates.name
-        position = self.linkStates.pose[names.index('robotis::body_link')].position
-        return position
+  def linkStatesCallback(self, linkStates):
+    self.linkStates = linkStates
+    states = linkStates
+
+  def getPelvisPosition(self):
+    names = self.linkStates.name
+    position = self.linkStates.pose[names.index('robotis::body_link')].position
+    return position
+
+  def getHandBoxDistance(self):
+    try:
+      linkState = self.gazeboLinkSrv.call("l_el_link","light_box")
+      x = linkState.link_state.pose.position.x
+      y = linkState.link_state.pose.position.y - (.125/2)
+      z = linkState.link_state.pose.position.z 
+      return np.sqrt(x**2 + y**2 + z**2)
+    except Exception:
+      return 0
